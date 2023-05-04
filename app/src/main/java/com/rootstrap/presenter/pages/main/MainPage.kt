5c6f02041bcefec288ad07231a7845a5066d84d0
Amaury <ricardo901130@gmail.com>
2023-05-04 14:38:25 -0300

    fix comment

--- app/src/main/java/com/rootstrap/presenter/pages/main/MainPage.kt
+++ app/src/main/java/com/rootstrap/presenter/pages/main/MainPage.kt
@@ -6,13 +6,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.rootstrap.presenter.pages.main.components.HelloUser
 
-/**
- * This is an example fo a classic drawer layout
- * **/
 @Composable
 fun MainPage() {
     rememberNavController()
