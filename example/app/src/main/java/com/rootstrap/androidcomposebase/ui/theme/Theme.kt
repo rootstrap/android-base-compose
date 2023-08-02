33cc7389676c90581247a0e99946a35bf139bd71
Rodrigo Soria <rodrgo.soria@rootstrap.com>
2023-08-02 14:36:54 -0400

    Added empty end lines on files mentioned by CR

--- example/app/src/main/java/com/rootstrap/androidcomposebase/ui/theme/Theme.kt
+++ example/app/src/main/java/com/rootstrap/androidcomposebase/ui/theme/Theme.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val LightColors = lightColorScheme(
